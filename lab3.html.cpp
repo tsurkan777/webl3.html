db50a623cde53fbe12a651bec584f2764cc58f71
tsurkan777 <tsurkan.sofiia@chnu.edu.ua>
2025-11-22 22:23:52 +0200

    Add initial HTML structure for library menu

---
+++ lab3.html.cpp
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <title>Меню бібліотеки</title>
+
+    <link rel="stylesheet" href="style.css" rel="stylesheet" charset="UTF-8" title="main-style">
+</head>
+
+<body style="font-family: Arial; background:#faf8f4;">
+
+<nav id="main-menu">
+    <h2>Головне меню бібліотеки</h2>
+
+    <menu title="Головне меню" class="top-menu" label="Меню">
+        <li><a href="lab2_part1.html" title="Поточна сторінка" style="color:#4b2e83;">Головна</a></li>
+        <li><a href="lists.html" title="Сторінка зі списками" rev="content" shape="rect">Списки бібліотеки</a></li>
+        <li><a href="lab2_part2.html" title="Про бібліотеку">Про бібліотеку</a></li>
+    </menu>
+</nav>
+
+<main class="content">
+    <marquee scrolldelay="70" truespeed="true" vspace="10" width="80%" hspace="10" style="color:#4b2e83; font-size:18px;">
+        Вітаємо у віртуальній бібліотеці BookWorld!
+    </marquee>
+
+    <section id="about">
+        <h3>Про бібліотеку</h3>
+        <p>
+            Наша бібліотека пропонує великий вибір літератури, комфортні читальні зали  
+            та сучасні електронні сервіси. Вітаємо кожного відвідувача!
+        </p>
+    </section>
+</main>
+
+</body>
+</html>
